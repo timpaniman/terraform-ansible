295306cf8cff248d538e853819250c38e3c67aee
Varun <varunmanik1@gmail.com>
2022-04-17 10:37:19 +0000

    terraform aws SG creation

--- main.tf
+++ main.tf
@@ -13,3 +13,41 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-sl"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_security_group" "allow_SSH" {
+  name        = "allow_SSH"
+  description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    # description      = "SSH from VPC"
+    # from_port        = 22
+    # to_port          = 22
+    # protocol         = "tcp"
+    # cidr_blocks      = ["61.6.14.46/32"]
+    # # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
