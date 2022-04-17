ab4d21aab3b3e6da62b155aaa496618497cbbd76
Varun <varunmanik1@gmail.com>
2022-04-17 10:39:15 +0000

    terraform aws SG creation-1

--- main.tf
+++ main.tf
@@ -14,14 +14,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-sl"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
 resource "aws_security_group" "allow_SSH" {
   name        = "allow_SSH"
