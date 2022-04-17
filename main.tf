5c2d296f8f8ee1aee32f1110e3ef1e90302ca1f2
Varun <varunmanik1@gmail.com>
2022-04-17 10:39:41 +0000

    terraform aws s3 creation-1

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-sl"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
