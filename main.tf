50ab6a8946db4d08653308a9c85f13cdce216a97
Varun <varunmanik1@gmail.com>
2022-05-14 14:22:17 +0000

    adding remote-exec commands

--- main.tf
+++ main.tf
@@ -77,6 +77,23 @@ resource "aws_instance" "ubuntu" {
     "ENV"  = "Dev"
   }
 
+  # Remotely execute commands to install Java, Python, Jenkins
+    provisioner "remote-exec" {
+      inline = [
+        "sudo apt update && upgrade",
+        "sudo apt install -y python3.8",
+        "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo
+        apt-key add -",
+        "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ >
+        /etc/apt/sources.list.d/jenkins.list'",
+        "sudo apt-get update",
+        "sudo apt-get install -y openjdk-8-jre",
+        "sudo apt-get install -y jenkins",
+      ]
+    }
+
   depends_on = [aws_key_pair.deployer1]
 
 }
+
+
