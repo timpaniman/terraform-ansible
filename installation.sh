621928622a23b32471ba5b533be33730abbe1529
Varun <varunmanik1@gmail.com>
2022-05-14 14:31:21 +0000

    adding remote-exec commands

---
+++ installation.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+sudo apt update && upgrade
+sudo apt install -y python3.8
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo
+apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jre
+sudo apt-get install -y jenkins
